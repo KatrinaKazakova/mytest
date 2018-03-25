019b327696f2e78a64a410141ea7eadb41b663fc
KatrinaKazakova <rus-katrina@hotmail.com>
2018-03-25 16:42:17 -0500

    first time

---
+++ src/test/java/test/com/DoctorJUnitTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.com;
+
+import com.itexps.mymavenproj.myjunitgit.Doctor;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ekaterina
+ */
+public class DoctorJUnitTest {
+    
+    Doctor doc;
+    
+    public DoctorJUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        
+        doc = new Doctor();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    @Test
+    public void getName() {
+        doc.setName("tom"); // setting name
+        assertEquals("tom", doc.getName()); // validating name
+    
+    }
+}
